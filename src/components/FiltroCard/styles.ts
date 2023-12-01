240b3383940baf318b8f31544313e0bba62215b8
Patrick Casimiro <patrickcasimiroz@gmail.com>
2023-12-01 15:26:36 -0300

    feat: adicionando e conhecendo o Omit

--- src/components/FiltroCard/styles.ts
+++ src/components/FiltroCard/styles.ts
@@ -2,7 +2,9 @@ import styled from 'styled-components'
 
 import { Props } from '.'
 
-export const Card = styled.div<Props>`
+type PropsSemContadorELegenda = Omit<Props, 'contador' | 'legenda'>
+
+export const Card = styled.div<PropsSemContadorELegenda>`
   padding: 8px;
   color: ${(props) => (props.ativo ? '#1E90FF' : '#5e5e5e')};
   border: 1px solid ${(props) => (props.ativo ? '#1E90FF' : '#a1a1a1')};

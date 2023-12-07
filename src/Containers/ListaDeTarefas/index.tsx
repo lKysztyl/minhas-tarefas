a52c639c8493b509e8768b293c48d432d1b2cf91
Patrick Casimiro <patrickcasimiroz@gmail.com>
2023-12-07 17:02:12 -0300

    feat: adicionando o card para tarefas

--- src/Containers/ListaDeTarefas/index.tsx
+++ src/Containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,27 @@
+import Tarefas from '../../components/Tarefa'
+import { Container } from './styles'
+
 const ListaDeTarefas = () => (
-  <main>
+  <Container>
     <p>2 tarefas marcadas como: &quot;cateforia&ldquo; e &quot;termo&ldquo;</p>
-  </main>
+    <ul>
+      <li>
+        <Tarefas />
+      </li>
+      <li>
+        <Tarefas />
+      </li>
+      <li>
+        <Tarefas />
+      </li>
+      <li>
+        <Tarefas />
+      </li>
+      <li>
+        <Tarefas />
+      </li>
+    </ul>
+  </Container>
 )
 
 export default ListaDeTarefas

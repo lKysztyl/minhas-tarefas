55ef7f1d2070a36a5c023f52eb36ce03fa856e4d
Patrick Casimiro <patrickcasimiroz@gmail.com>
2024-01-03 16:55:21 -0300

    Adicionando logica de cores para a lista

--- src/Containers/ListaDeTarefas/index.tsx
+++ src/Containers/ListaDeTarefas/index.tsx
@@ -1,25 +1,41 @@
 import Tarefas from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Castelo de Vidro',
+    descricao: 'Livro de Fantasia',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Duna',
+    descricao: 'Livro de Fantasia',
+    prioridade: 'urgente',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Rust',
+    descricao: 'Livro de Rust',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como: &quot;cateforia&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefas />
-      </li>
-      <li>
-        <Tarefas />
-      </li>
-      <li>
-        <Tarefas />
-      </li>
-      <li>
-        <Tarefas />
-      </li>
-      <li>
-        <Tarefas />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefas
+            descricao={t.descricao}
+            prioridade={t.prioridade}
+            status={t.status}
+            titulo={t.titulo}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
--- src/components/Tarefa/styles.ts
+++ src/components/Tarefa/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import variaveis from '../../styles/variaveis'
 
 export const Card = styled.div`
   background-color: #fcfcfc;
@@ -14,12 +15,29 @@ export const Titulo = styled.h3`
   margin-bottom: 16px;
 `
 
-export const Tag = styled.span`
+type TagProps = {
+  prioridade?: string
+  status?: string
+}
+
+function retornaCorDeFundo(props: TagProps): string {
+  if ('status' in props) {
+    if (props.status === 'pendente') return variaveis.yellow
+    if (props.status === 'concluído') return variaveis.green
+  } else if ('prioridade' in props) {
+    if (props.prioridade === 'urgente') return variaveis.yellow
+    if (props.prioridade === 'importante') return variaveis.orange
+  }
+
+  return '#ccc'
+}
+
+export const Tag = styled.span<TagProps>`
   padding: 4px 8px;
   color: #fff;
   font-weight: bold;
   font-size: 10px;
-  background-color: #e1a32a;
+  background-color: ${(props) => retornaCorDeFundo(props)};
   border-radius: 8px;
   margin-right: 16px;
   display: inline-block;
@@ -53,4 +71,12 @@ export const Botao = styled.button`
   background-color: #2f3640;
   border-radius: 8px;
   margin-right: 8px;
+`
+
+export const BotaoSalvar = styled(Botao)`
+  background-color: ${variaveis.green};
+`
+
+export const BotaoCancelarRemover = styled(Botao)`
+  background-color: ${variaveis.red};
 `

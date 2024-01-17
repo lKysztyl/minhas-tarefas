621857817e94dfa558d56c9c10d52eab7fb1cc08
Patrick Casimiro <patrickcasimiroz@gmail.com>
2024-01-16 21:46:07 -0300

    feat: terminado a imlementação do Redux para a filtragem de tarefas

--- src/Containers/BarraLateral/index.tsx
+++ src/Containers/BarraLateral/index.tsx
@@ -1,10 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { RootReducer } from '../../store'
+import FiltroCard from '../../components/FiltroCard'
+import { alterarTermo } from '../../store/reducers/filtros'
 
 import * as S from './styles'
 
-import FiltroCard from '../../components/FiltroCard'
-import { alterarTermo } from '../../store/reducers/filtros'
+import * as enums from '../../utils/enums/Tarefa'
 
 const BarraLateral = () => {
   const dispatch = useDispatch()
@@ -19,12 +20,32 @@ const BarraLateral = () => {
         onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
       />
       <S.Filtros>
-        <FiltroCard legenda="pendentes" contador={1} />
-        <FiltroCard legenda="concluídas" contador={2} />
-        <FiltroCard legenda="urgentes" contador={3} />
-        <FiltroCard legenda="importantes" contador={4} />
-        <FiltroCard legenda="normal" contador={5} />
-        <FiltroCard legenda="todas" contador={10} ativo />
+        <FiltroCard
+          valor={enums.Status.PENDENTE}
+          criterio="status"
+          legenda="pendentes"
+        />
+        <FiltroCard
+          valor={enums.Status.PENDENTE}
+          criterio="status"
+          legenda="concluídas"
+        />
+        <FiltroCard
+          valor={enums.Prioridade.URGENTE}
+          criterio="prioridade"
+          legenda="urgentes"
+        />
+        <FiltroCard
+          valor={enums.Prioridade.IMPORTANTE}
+          criterio="prioridade"
+          legenda="importantes"
+        />
+        <FiltroCard
+          valor={enums.Prioridade.NORMAL}
+          criterio="prioridade"
+          legenda="normal"
+        />
+        <FiltroCard criterio="todas" legenda="todas" />
       </S.Filtros>
     </S.Aside>
   )
